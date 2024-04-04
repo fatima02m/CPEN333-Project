d129a61a4f1d78a736dc9dd8a11c0fc755dfa4f3
Ethan <ethan.watchorn@gmail.com>
2024-04-04 09:56:13 -0700

    Started part 2

---
+++ main.py
@@ -0,0 +1,14 @@
+#Content of main.py; use as is
+from tkinter import *
+import multiprocessing
+
+import projectPart2_client as client
+import projectPart2_server as server
+
+if __name__ == "__main__":
+    server = multiprocessing.Process(target=server.main)
+    client1 = multiprocessing.Process(target=client.main, name="Client1")
+    client2 = multiprocessing.Process(target=client.main, name="Client2")
+    server.start()
+    client1.start()
+    client2.start()
